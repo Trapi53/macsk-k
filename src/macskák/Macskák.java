d0fc87d39bac64314d814f4256a0df7dfcde437e
ASDleves <ricsi20020302@gmail.com>
2022-03-07 15:32:26 +0100

    javitott

--- src/macskák/Macskák.java
+++ src/macskák/Macskák.java
@@ -1,7 +1,9 @@
 package macskák;
 public class Macskák {
     public static void main(String[] args) {
-        String nev1="Trapi", nev2, nev3;
+        String nev1="Trapi"; 
+        String nev2="Ricsi";
+        String nev3="nev3";
     }
     
 }
